113faffb0198406de369af97745118f7058fc1b2
hoomanist <hham.group12@gmail.com>
2021-10-12 18:00:49 +0330

    project initialize

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  sources = import ./nix/sources.nix;
+  rust = import ./nix/rust.nix { inherit sources; };
+  pkgs = import sources.nixpkgs { };
+in
+
+pkgs.mkShell {
+  buildInputs = [
+    rust
+
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+}
