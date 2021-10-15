2ca532e6fa12d2b53c10ffa4e6692fdc58918dee
hoomanist <hham.group12@gmail.com>
2021-10-15 16:19:08 +0330

    is empty

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
 mod stack;
 
 fn main() {
-    let mut stack = stack::new_stack();
-    stack.dump();
-    stack.push(12);
-    stack.push(16);
-    stack.push(18);
-    stack.push(11);
-    stack.dump();
-    println!("****");
-    stack.pop();
-    stack.dump();
-
 }
--- src/stack.rs
+++ src/stack.rs
@@ -11,18 +11,21 @@ enum Pointer{
 #[derive(Clone)]
 pub struct Stack {
     list: Vec<Node>,
-    head: Pointer,
+    stack_pointer: Pointer,
 }
 
 pub fn new_stack() -> Stack {
    let l = Vec::new();
    Stack{
     list: l,
-    head: Pointer::Empty,
+    stack_pointer: Pointer::Empty,
    }
 }
 
 impl Stack {
+    pub fn is_empty(&mut self) -> bool {
+        self.list.is_empty()
+    }
     pub fn dump(&mut self) -> Vec<i32> {
         let mut dumped = Vec::new();
         for node in self.list.iter() {
@@ -40,20 +43,20 @@ impl Stack {
                 data: data,
             };
         } else {
-            let head = self.head.clone();
+            let stack_pointer= self.stack_pointer.clone();
             node = Node{
-                next: head,
+                next: stack_pointer,
                 data: data,
             };
         }
         self.list.push(node.clone());
-        self.head = Pointer::Pointy(Box::new(node));
+        self.stack_pointer = Pointer::Pointy(Box::new(node));
     }
     pub fn pop(&mut self) -> Node {
         let current_haed = self.list[self.list.len() - 1].clone();
         self.list.pop();
         let new_head = self.list[self.list.len() - 1].clone();
-        self.head = Pointer::Pointy(Box::new(new_head));
+        self.stack_pointer = Pointer::Pointy(Box::new(new_head));
         current_haed
     }
     
@@ -70,9 +73,7 @@ mod tests {
         let mut stack = new_stack();
         stack.push(1);
         stack.push(15);
-        let mut expected = Vec::new();
-        expected.push(1);
-        expected.push(15);
+        let expected = vec![1, 15];
         assert_eq!(expected, stack.dump());
     }
 
@@ -82,12 +83,19 @@ mod tests {
         stack.push(1);
         stack.push(15);
         stack.push(42);
-        let latest = stack.pop();
-        let mut expected = Vec::new();
-        expected.push(1);
-        expected.push(15);
+        stack.push(121);
+        assert_eq!(stack.pop().data, 121);
+        assert_eq!(stack.pop().data, 42);
+        let expected = vec![1, 15];
         assert_eq!(expected, stack.dump());
-        assert_eq!(latest.data, 42)
     }
 
+    #[test]
+    fn test_is_empty() {
+        let mut stack = new_stack();
+        assert_eq!(stack.is_empty(), true);
+        stack.push(42);
+        assert_eq!(stack.is_empty(), false);
+    }
 }
+
